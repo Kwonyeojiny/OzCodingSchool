682ab77dc686d259e097445609c171125e153ac7
Kwonyeojiny <kangagi0009@naver.com>
2024-10-14 11:59:51 +0900

    React_day4 props 사용

--- React/React_day4/vite_day4/src/App.jsx
+++ React/React_day4/vite_day4/src/App.jsx
@@ -1,43 +1,61 @@
-//import { useState } from 'react'
-import { Component } from 'react'
+import { useState } from 'react'
+//import { Component } from 'react'
 import './App.css'
 
-// function App() {
-//   const[counter,setCounter] = useState(0)
-//   return (
-//     <>
-//       <div>counter : {counter}</div>
-//       <button 
-//         onClick={()=>{
-//           setCounter((prev)=> prev+1)
-//           // setCounter(counter + 1)
-//         }}>+</button>
-//       <button 
-//         onClick={()=>{
-//           setCounter((prev)=> prev-1)
-//           // setCounter(counter - 1)
-//         }}>-</button>
-//     </>
-//   )
-// }
-
-class App extends Component {
-  state = {counter:0};
-  render(){
-    return(
-      <>
-        <div>counter {this.state.counter}</div>
-        <button 
+function App() {
+  const[counter,setCounter] = useState(0)
+  return (
+    <>
+      <Count counter={counter} hello={"hello"} array={[1,2,3,"안녕하세요"]}/>
+      <button 
         onClick={()=>{
-          this.setState({counter : this.state.counter + 1})
+          setCounter((prev)=> prev+1)
+          // setCounter(counter + 1)
         }}>+</button>
       <button 
         onClick={()=>{
-          this.setState({counter : this.state.counter - 1})
+          setCounter((prev)=> prev-1)
+          // setCounter(counter - 1)
         }}>-</button>
-      </>
-    );
-  }
+    </>
+  )
 }
+
+function Count ({array, counter, hello}){
+  console.log("array", array);
+  console.log("counter",counter);
+  console.log("hello",hello);
+  return <div>counter : {counter}</div>
+}
+
+// class App extends Component {
+//   state = {counter:0};
+//   render(){
+//     return(
+//       <>
+//         <Count counter={this.state.counter}/>
+//         <button 
+//         onClick={()=>{
+//           this.setState({counter : this.state.counter + 1})
+//         }}>+</button>
+//       <button 
+//         onClick={()=>{
+//           this.setState({counter : this.state.counter - 1})
+//         }}>-</button>
+//       </>
+//     );
+//   }
+// }
+
+// class Count extends Component{
+//   // constructor(props){
+//   //   super(props)
+//   // }
+
+//   render(){
+//     console.log("props",this.props)
+//     return <div>counter : {this.props.counter}</div>
+//   }
+// }
 
 export default App
