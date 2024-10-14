4d0cf2eeb07e4804100baa1d77227a48146bef99
Kwonyeojiny <kangagi0009@naver.com>
2024-10-14 21:51:21 +0900

    React_day4 상태끌어올리기

--- React/React_day4/vite_day4/src/App.jsx
+++ React/React_day4/vite_day4/src/App.jsx
@@ -4,46 +4,113 @@ import './App.css'
 
 function App() {
   const[counter,setCounter] = useState(0)
+  const[inputValue, setInputValue] = useState(1)
+
+  const incrementCounter = () => {
+    setCounter(counter + 1)
+  }
+
+  const decrementCounter = () => {
+    setCounter(counter - 1)
+  }
+
+  const setCounterNumber = () => {
+    setCounter(inputValue)
+  }
+
   return (
     <>
       <Count counter={counter} hello={"hello"} array={[1,2,3,"안녕하세요"]}/>
-      <button 
-        onClick={()=>{
-          setCounter((prev)=> prev+1)
-          // setCounter(counter + 1)
-        }}>+</button>
-      <button 
-        onClick={()=>{
-          setCounter((prev)=> prev-1)
-          // setCounter(counter - 1)
-        }}>-</button>
+      <PlusButton setCounter={setCounter} incrementCounter={incrementCounter}/>
+      <MinusButton setCounter={setCounter} decrementCounter={decrementCounter} />
+      <CounterInput inputValue={inputValue} setInputValue={setInputValue} setCounter={setCounter} setCounterNumber={setCounterNumber}/>
     </>
   )
 }
 
-function Count ({array, counter, hello}){
-  console.log("array", array);
-  console.log("counter",counter);
-  console.log("hello",hello);
+function CounterInput({inputValue, setInputValue, setCounter, setCounterNumber}){
+  return(
+    <>
+      <input 
+        type='number' 
+        value={inputValue} 
+        onChange={(e)=>setInputValue(e.target.value)}/>
+      <button 
+        // onClick={
+        // ()=> setCounter(inputValue)}
+        onClick={setCounterNumber}
+         >입력</button>
+    </>
+  )
+}
+
+function PlusButton({setCounter, incrementCounter}){
+  return (
+    <button 
+      // onClick={()=>{
+      //   setCounter((prev) => prev + 1 )
+      // }}
+      onClick={incrementCounter}
+    >+</button>
+  )
+}
+
+function MinusButton({setCounter, decrementCounter}){
+  return (
+    <button 
+      // onClick={()=>{
+      //   setCounter((prev) => prev - 1 )
+      // }}
+      onClick={decrementCounter}
+    >-</button>
+  )
+}
+
+function Count ({counter}){
   return <div>counter : {counter}</div>
 }
 
 // class App extends Component {
 //   state = {counter:0};
+
+//   incrementCounter = () => {
+//     this.setState({counter: this.state.counter +1})
+//   }
+//   decrementCounter = () => {
+//     this.setState({counter: this.state.counter -1})
+//   }
+
 //   render(){
 //     return(
 //       <>
 //         <Count counter={this.state.counter}/>
-//         <button 
-//         onClick={()=>{
-//           this.setState({counter : this.state.counter + 1})
-//         }}>+</button>
-//       <button 
-//         onClick={()=>{
-//           this.setState({counter : this.state.counter - 1})
-//         }}>-</button>
+//         <PlusButton incrementCounter={this.incrementCounter}/>
+//         <MinusButton/>
 //       </>
 //     );
+//   }
+// }
+
+// class PlusButton extends Component {
+//   render(){
+//     return (
+//       <button 
+//         onClick={
+//           this.props.incrementCounter
+//         }>+</button>
+//     )
+//   }
+// }
+
+// class MinusButton extends Component {
+//   render(){
+//     return (
+//       <button 
+//         onClick={
+//           this.props.decrementCounter
+//         }>-</button>
+      
+//     )
 //   }
 // }
 
@@ -53,7 +120,6 @@ function Count ({array, counter, hello}){
 //   // }
 
 //   render(){
-//     console.log("props",this.props)
 //     return <div>counter : {this.props.counter}</div>
 //   }
 // }
