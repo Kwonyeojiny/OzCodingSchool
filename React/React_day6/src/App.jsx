02f1c24536409e797cd44b66c99da86b86e448ba
Kwonyeojiny <kangagi0009@naver.com>
2024-10-16 00:15:09 +0900

    React_day6 조건부 렌더링

---
+++ React/React_day6/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [mood, setMood] = useState('Normal')
+  return (
+    <>
+      <Face3 mood={mood}/>
+      <div className={mood === "Happy" ? 'happy' :
+        mood === "Normal" ? 'normal' : 'sad'
+      }>기분 : {mood}</div>
+      <div>
+        <button onClick={()=> setMood("Happy")}>Happy</button>
+        <button onClick={()=> setMood("Normal")}>Normal</button>
+        <button onClick={()=> setMood("Sad")}>Sad</button>
+      </div>
+    </>
+  )
+}
+
+// 1. if문으로 리턴하는 JSX문 바꿔주기
+function Face({mood}) {
+  if(mood === 'Happy'){
+    return ("😊")
+  }
+  else if (mood === "Normal") {
+    return ("😐")
+  }
+  else {
+    return ("😭")
+  }
+}
+
+// 2. 삼항연산자 사용하기
+function Face2({mood}) {
+  return (
+    <>
+      {mood === "Happy" ? 
+        (<p>😊</p>) : mood === "Normal" ? 
+        (<div>😐</div>) : (<div>😭</div>)}
+    </>
+  )
+}
+
+// 3. 논리 연산자
+function Face3 ({mood}) {
+  return (
+    <>
+      {mood === "Happy" && <div>😊</div>}
+      {mood === "Normal" && <div>😐</div>}
+      {mood === "Sad" && <div>😭</div>}
+    </>
+  )
+}
+
+export default App
