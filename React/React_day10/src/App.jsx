a235dffef16fc463bb5135c7bdbd5e87a0133494
Kwonyeojiny <kangagi0009@naver.com>
2024-10-22 14:25:10 +0900

    React_day10 @mixin @include

--- React/React_day10/src/App.jsx
+++ React/React_day10/src/App.jsx
@@ -10,26 +10,17 @@ function App() {
       <div className='test' >테스트용 div입니다</div>
       <h2>SCSS 변수 연습</h2>
       <ul>
-        <li>
-          안녕하세요
-          <p>p요소입니다</p>
-        </li>
-        <li>
-          반갑습니다
-          <section>Section요소입니다</section>
-        </li>
-        <li>
-          SCSS
-          <article>
-            article요소입니다
-            <h3>h3요소입니다</h3>
-          </article>
-        </li>
-        <li>
-          공부해봅시다
-          <h2>h2요소입니다</h2>
-        </li>
+        <li>안녕하세요</li>
+        <li>반갑습니다</li>
+        <li>SCSS</li>
+        <li>공부해봅시다</li>
       </ul>
+      <section>
+        <article>SCSS문법에는</article>
+        <article>변수 사용과</article>
+        <article>중접 문법과</article>
+        <article>믹스인 등이 있습니다</article>
+      </section>
     </>
   )
 }
